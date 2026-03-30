8e50de1fea51ca19ad4ef399ff5613012b2ea001
Joaquin Antonio Ortiz Gamboa <FoxFire3713@gmail.com>
2026-03-30 12:46:16 -0500

    New

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    base: '/InHousePeru-Landing1',
+    site: 'https://DoubleLiffT.github.io',
+    vite: {
+        plugins: [tailwindcss()],
+    },
+});
